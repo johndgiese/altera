47058f470b10dd7d3b07a17db95d7c9036e43131
J. David Giese <johndgiese@gmail.com>
2022-03-27 14:14:06 -0500

    Implement the code that grabs people's views

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+mod modulo;
+use modulo::Mod;
+
+use std::cmp;
 use std::collections::HashMap;
 
 fn main() {
-    // if file provided, load the world from it
-    // else create a single nook world
+    // TODO: if file provided, load the world from it, else create world of specified size
     let mut world = single_nook_world(5, 5);
-    // step for the specified number of steps, printing after each one
-    world.print();
-    world.step();
-    world.print();
-    // save the final file to the specified location
+    let num_steps = 10;
+    for i in 1..num_steps {
+        println!("Step {}", i);
+        world.print();
+        world.step();
+    }
+    // TODO: save the final file to the specified location
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -18,7 +23,7 @@ struct Nook {
 
 type View = [[u8; 5]; 5];
 
-type Position = (usize, usize);
+type Position = (isize, isize);
 
 impl Nook {
     fn act(&self, view: View) -> Action {
@@ -50,14 +55,15 @@ enum Action {
 }
 
 struct World {
-    nx: usize,
-    ny: usize,
+    nx: isize,
+    ny: isize,
     things: HashMap<Position, Thing>,
 }
 
 impl World {
     fn step(&mut self) {
-        let mut actions: Vec<Action> = Vec::with_capacity(self.num_nooks());
+        let num_actions = self.num_nooks().try_into().unwrap();
+        let mut actions: Vec<Action> = Vec::with_capacity(num_actions);
         for (position, thing) in self.things.iter() {
             match thing {
                 Thing::Nook(nook) => {
@@ -71,18 +77,29 @@ impl World {
         self.apply_actions(actions);
     }
 
-    fn get_view(&self, p: &Position) -> View {
-        // TODO: implement this
-        [
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0],
-        ]
+    fn get_view(&self, center: &Position) -> View {
+        let mut view: View = [[0u8; 5]; 5];
+        let yc = center.0;
+        let xc = center.1;
+        for (iy, y) in (-2..=2).enumerate() {
+            for (ix, x) in (-2..=2).enumerate() {
+                let yp = (yc + y).modulo(self.ny);
+                let xp = (xc + x).modulo(self.nx);
+                view[iy][ix] = self.get_weight(&(yp, xp));
+            }
+        }
+        view
     }
 
-    fn total_weight(&self) -> usize {
+    fn get_weight(&self, p: &Position) -> u8 {
+        match self.things.get(p) {
+            None => 0,
+            Some(Thing::Nook(nook)) => nook.weight,
+            Some(Thing::Food) => 1,
+        }
+    }
+
+    fn total_weight(&self) -> u128 {
         self.things.values().fold(0, |sum, thing| {
             sum + match thing {
                 Thing::Nook(nook) => nook.weight.into(),
@@ -91,7 +108,7 @@ impl World {
         })
     }
 
-    fn num_nooks(&self) -> usize {
+    fn num_nooks(&self) -> u128 {
         self.things.values().fold(0, |sum, thing| {
             sum + match thing {
                 Thing::Nook(_) => 1,
@@ -105,14 +122,14 @@ impl World {
     }
 
     fn print(&self) {
-        for x in 0..self.nx {
-            for y in 0..self.ny {
-                let position = (x, y);
+        for y in 0..self.ny {
+            for x in 0..self.nx {
+                let position = (y, x);
                 let thing = self.things.get(&position);
                 match thing {
-                    None => print!("."),
-                    Some(Thing::Food) => print!("f"),
-                    Some(Thing::Nook(_)) => print!("n"),
+                    None => print!(" "),
+                    Some(Thing::Food) => print!("."),
+                    Some(Thing::Nook(nook)) => print!("{}", cmp::min(nook.weight, 9)),
                 }
             }
             println!("");
@@ -120,20 +137,34 @@ impl World {
     }
 }
 
-fn single_nook_world(nx: usize, ny: usize) -> World {
+fn single_nook_world(nx: isize, ny: isize) -> World {
     let mut things = HashMap::new();
-    for x in 0..nx {
-        for y in 0..ny {
+    for y in 0..ny {
+        for x in 0..nx {
             let thing;
             if x == (nx - 1) / 2 && y == (ny - 1) / 2 {
                 thing = Thing::Nook(Nook { weight: 1 });
             } else {
                 thing = Thing::Food;
             }
-            things.insert((x, y), thing);
+            things.insert((y, x), thing);
         }
     }
-    World { nx, ny, things }
+    World { ny, nx, things }
+}
+
+fn world_from_func<F>(ny: isize, nx: isize, func: F) -> World
+where
+    F: Fn(Position) -> Thing,
+{
+    let mut things = HashMap::new();
+    for y in 0..ny {
+        for x in 0..nx {
+            let position = (y, x);
+            things.insert(position, func(position));
+        }
+    }
+    World { ny, nx, things }
 }
 
 #[test]
@@ -154,4 +185,33 @@ fn test_total_weight() {
     assert_eq!(single_nook_world(2, 1).total_weight(), 2);
     assert_eq!(single_nook_world(2, 2).total_weight(), 4);
     assert_eq!(single_nook_world(3, 3).total_weight(), 9);
+}
+
+#[test]
+fn test_get_view() {
+    let mut world = world_from_func(10, 10, |(_, x)| {
+        Thing::Nook(Nook {
+            weight: x.try_into().unwrap_or(0),
+        })
+    });
+    assert_eq!(
+        world.get_view(&(2, 2)),
+        [
+            [0, 1, 2, 3, 4],
+            [0, 1, 2, 3, 4],
+            [0, 1, 2, 3, 4],
+            [0, 1, 2, 3, 4],
+            [0, 1, 2, 3, 4],
+        ]
+    );
+    assert_eq!(
+        world.get_view(&(2, 0)),
+        [
+            [8, 9, 0, 1, 2],
+            [8, 9, 0, 1, 2],
+            [8, 9, 0, 1, 2],
+            [8, 9, 0, 1, 2],
+            [8, 9, 0, 1, 2],
+        ]
+    );
 }
---
+++ src/modulo.rs
@@ -0,0 +1,60 @@
+use std::ops::{Add, Rem};
+/// Trait that describes modulo operation
+/// # Examples
+///
+/// ```¨
+/// use modulo::Mod;
+/// fn main() {
+///     assert_eq!(4, (-2).modulo(6));
+///     assert_eq!(2, 9.modulo(7));
+/// }
+/// ```
+pub trait Mod<RHS = Self> {
+    type Output;
+    fn modulo(self, rhs: RHS) -> Self::Output;
+}
+
+impl<A, B, C> Mod<B> for A
+where
+    A: Rem<B, Output = C>,
+    B: Clone,
+    C: Add<B, Output = C> + Default + PartialOrd,
+{
+    type Output = C;
+    fn modulo(self, rhs: B) -> Self::Output {
+        let result = self % rhs.clone();
+        if result < Self::Output::default() {
+            result + rhs
+        } else {
+            result
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Mod;
+
+    #[test]
+    fn smaller_than_modulus_works() {
+        assert_eq!(3, 3.modulo(9));
+        assert_eq!(7, 7.modulo(8));
+    }
+
+    #[test]
+    fn larger_than_modulus_works() {
+        assert_eq!(1, 6.modulo(5));
+        assert_eq!(8, 18.modulo(10));
+    }
+
+    #[test]
+    fn megative_works() {
+        assert_eq!(1, (-1).modulo(2));
+        assert_eq!(2, (-7).modulo(3));
+    }
+
+    #[test]
+    fn big_number_works() {
+        assert_eq!(1, 101.modulo(10));
+    }
+}
